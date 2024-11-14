e7a7800f2b078abf4c0af5c91d3b9dd1168b5529
Kevin A. Naudé <1943178+kevin-in-code@users.noreply.github.com>
2024-11-14 22:16:50 +1100

    Add version for TypeScript with vitest

---
+++ ts-vitest/test/gilded-rose.test.ts
@@ -0,0 +1,11 @@
+import { assert, describe, expect, it, test } from 'vitest';
+
+import {Shop, Item} from "../src/gilded-rose";
+
+describe("Gilded Rose", function() {
+  it("should foo", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("fixme");
+  });
+});
